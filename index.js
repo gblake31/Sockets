00e2ba84f6aa677ccc9a3e374acd86ad1ea2d2e2
Blake Gassman <51721851+gblake31@users.noreply.github.com>
2023-05-02 23:23:25 -0400

    Different Port for Deployment

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const port = process.env.PORT || 5000;
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/chat.html');
@@ -26,6 +28,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log('listening on *:5000');
+server.listen(port, () => {
+  console.log('listening on *:' + port);
 });
