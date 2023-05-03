904b9e6dd977c06d56f30c0dace894e9eb94cf62
Blake Gassman <51721851+gblake31@users.noreply.github.com>
2023-05-02 22:24:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const io = new Server(server);
 
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/chat.html');
 });
 
 app.get('/hello', (req, res) => {
