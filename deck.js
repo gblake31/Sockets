0bd37023de91513f73de0a0649a07fcf7444420b
Blake Gassman <51721851+gblake31@users.noreply.github.com>
2023-08-25 16:17:30 -0400

    Migrating to Typescript was a disaster. Reset!!!

--- deck.js
+++ deck.js
@@ -1,37 +1,41 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Card = exports.Deck = void 0;
-var Deck = /** @class */ (function () {
-    function Deck(shuffled) {
+class Deck {
+    constructor(shuffled) {
         this.numCards = 52;
         this.cards = [];
-        for (var i = 0; i < 52; i++) {
+
+        for (let i = 0; i < 52; i++) {
             this.cards.push(new Card(i));
         }
+    
         if (shuffled) {
             this.shuffle();
         }
+
     }
-    Deck.prototype.drawCard = function () {
+
+    drawCard() {
         this.numCards--;
-        var card = this.cards[this.numCards];
+        let card = this.cards[this.numCards];
         return card;
-    };
-    Deck.prototype.shuffle = function () {
+    }
+
+    shuffle() {
         // Simulate shuffling by taking two random indices and swapping their contents.
-        for (var s = 0; s < 500; s++) {
-            var i = Math.floor(Math.random() * 52);
-            var j = Math.floor(Math.random() * 52);
-            var temp = this.cards[i];
+        for (let s = 0; s < 500; s++) {
+            let i = Math.floor(Math.random() * 52);
+            let j = Math.floor(Math.random() * 52);
+            
+            let temp = this.cards[i];
             this.cards[i] = this.cards[j];
             this.cards[j] = temp;
         }
-    };
-    return Deck;
-}());
-exports.Deck = Deck;
-var Card = /** @class */ (function () {
-    function Card(id) {
+    }
+
+}
+
+class Card {
+
+    constructor(id) {
         this.id = id;
         this.rank = id % 13 + 2;
         this.rankStr = Card.findRankString(this.rank);
@@ -39,40 +43,34 @@ var Card = /** @class */ (function () {
         this.suitStr = Card.findSuitString(this.suit);
         this.imgStr = "/images/" + this.rankStr + "_of_" + this.suitStr + ".png";
     }
-    Card.findRankString = function (rankNum) {
+
+    static findRankString(rankNum) {
         if (rankNum == 11) {
             return "jack";
-        }
-        else if (rankNum == 12) {
+        } else if (rankNum == 12) {
             return "queen";
-        }
-        else if (rankNum == 13) {
+        } else if (rankNum == 13) {
             return "king";
-        }
-        else if (rankNum == 14) {
+        } else if (rankNum == 14) {
             return "ace";
-        }
-        else {
+        } else {
             return "" + rankNum;
         }
-    };
-    Card.findSuitString = function (suitNum) {
+    }
+      
+    static findSuitString(suitNum) {
         if (suitNum == 0) {
             return "hearts";
-        }
-        else if (suitNum == 1) {
+        } else if (suitNum == 1) {
             return "spades";
-        }
-        else if (suitNum == 2) {
+        } else if (suitNum == 2) {
             return "diamonds";
-        }
-        else if (suitNum == 3) {
+        } else if (suitNum == 3) {
             return "clubs";
-        }
-        else {
+        } else {
             return "invalid";
         }
-    };
-    return Card;
-}());
-exports.Card = Card;
+    }
+}
+
+module.exports = {Card, Deck};
--- index.js
+++ index.js
@@ -1,214 +1,250 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var socket_io_1 = require("socket.io");
-var deck_1 = require("./deck");
-var express = require('express');
-var app = express();
-var http = require('http');
-var server = http.createServer(app);
-var io = new socket_io_1.Server();
-var port = process.env.PORT || 5000;
-var States;
-(function (States) {
-    States[States["preflop"] = 0] = "preflop";
-    States[States["flop"] = 1] = "flop";
-    States[States["turn"] = 2] = "turn";
-    States[States["river"] = 3] = "river";
-    States[States["end"] = 4] = "end";
-})(States || (States = {}));
-;
-var bigBlind = 2;
-var users = [];
-var hands = [];
-var d;
-var curTurn = 0;
-var pot = 0;
-var toCall = bigBlind;
-var state = States.preflop;
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const deckModule = require('./deck');
+
+const port = process.env.PORT || 5000;
+
+const States = {
+  preflop: 0,
+  flop: 1,
+  turn: 2,
+  river: 3,
+  end: 4
+};
+
+const bigBlind = 2;
+
+let users = [];
+let hands = [];
+
+let d = {};
+let curTurn = 0;
+let pot = 0;
+let toCall = bigBlind;
+
+let state = States.preflop;
+
 // Express
 app.use("/images", express.static(__dirname + '/images'));
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/login.html');
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
 });
-app.get('/login.css', function (req, res) {
-    res.sendFile(__dirname + "/login.css");
-});
-app.get('/game.css', function (req, res) {
-    res.sendFile(__dirname + "/game.css");
-});
-app.get('/chat', function (req, res) {
-    res.sendFile(__dirname + '/chat.html');
-});
-app.get('/game', function (req, res) {
-    res.sendFile(__dirname + '/game.html');
-});
-var deckModule = require('./deck.js');
-app.get('/drawCard', function (req, res) {
-    var d = new deckModule.Deck(true);
-    var card = d.drawCard();
-    res.send("<img src =" + card.imgStr + ">");
-});
+app.get('/login.css', (req, res) => {
+  res.sendFile(__dirname + "/login.css");
+})
+app.get('/game.css', (req, res) => {
+  res.sendFile(__dirname + "/game.css");
+})
+app.get('/game', (req, res) => {
+  res.sendFile(__dirname + '/game.html');
+})
+app.get('/hello', (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+})
+
 // Socket IO Logic
-io.on('connection', function (socket) {
-    users.push(socket);
+io.on('connection', (socket) => {
+  users.push(socket);
+  io.emit('updateNumUsers', users.length);
+  console.log("connection");
+  
+  // Detecting emmisions from an arbitrary socket.
+  socket.on('register', (name) => {
+    socket.data.nickname = name;
+    console.log(name + " joined the game.");
+    io.emit('message', socket.data.nickname + " joined the chat.");
+  })
+  
+  socket.on('disconnect', () => {
+    users = users.filter(x => x.id != socket.id);
+    io.emit('message', socket.data.nickname + " disconnected.");
     io.emit('updateNumUsers', users.length);
-    // Detecting emmisions from an arbitrary socket.
-    socket.on('register', function (name) {
-        socket.data.nickname = name;
-        console.log(name + " joined the game.");
-        io.emit('message', socket.data.nickname + " joined the chat.");
-    });
-    socket.on('disconnect', function () {
-        users = users.filter(function (x) { return x.id != socket.id; });
-        io.emit('message', socket.data.nickname + " disconnected.");
-        io.emit('updateNumUsers', users.length);
-    });
-    socket.on('blake_message', function (msg) {
-        io.emit('message', socket.data.nickname + ": " + msg);
-    });
-    // Game actions
-    socket.on('start', function () {
-        d = new deck_1.Deck(true);
-        for (var i = 0; i < users.length; i++) {
-            hands[i] = [];
-            hands[i][0] = d.drawCard();
-        }
-        for (var i = 0; i < users.length; i++) {
-            hands[i][1] = d.drawCard();
-        }
-        for (var i = 0; i < users.length; i++) {
-            users[i].emit('deal', hands[i]);
-        }
-        curTurn = 0;
-        for (var i = 0; i < users.length; i++) {
-            users[i].data.money = 100;
-            users[i].data.folded = false;
-            users[i].bet = 0;
-            var obj = { money: users[i].data.money, toCall: toCall };
-            users[i].emit('updateTurn', users[0].data.nickname);
-            users[i].emit('updatePlayer', obj);
-            users[curTurn].emit('yourTurn');
-        }
-    });
-    socket.on('fold', function () {
-        console.log("FOLD");
-        socket.data.folded = true;
-        onMove(socket);
-    });
-    socket.on('call', function () {
-        if (socket.data.money < toCall) {
-            console.log("you can't do that.");
-            return;
-        }
-        // move their money into the pot.
-        socket.data.money -= toCall;
-        pot += toCall;
-        onMove(socket);
-    });
-    socket.on('raise', function (amt) {
-        if (socket.data.money < toCall) {
-            console.log("you can't do that.");
-            return;
-        }
-        socket.data.money -= amt;
-        pot += amt;
-        toCall += amt;
-        onMove(socket);
-    });
+  });
+  socket.on('blake_message', (msg) => {
+    io.emit('message', socket.data.nickname + ": " + msg);
+  })
+
+  // Game actions
+  socket.on('start', () => {
+    d = new deckModule.Deck(true);
+    for (let i = 0; i < users.length; i++) {
+      hands[i] = [];
+      hands[i][0] = d.drawCard();
+    }
+    for (let i = 0; i < users.length; i++) {
+      hands[i][1] = d.drawCard();
+    }   
+
+    for (let i = 0; i < users.length; i++) {
+      users[i].emit('deal', hands[i]);
+    }
+
+    curTurn = 0;
+    for (let i = 0; i < users.length; i++) {
+      users[i].data.money = 100;
+      users[i].data.folded = false;
+      users[i].bet = 0;
+
+      let obj = {money: users[i].data.money, toCall: toCall};
+
+      users[i].emit('updateTurn', users[0].data.nickname);
+      users[i].emit('updatePlayer', obj);
+      users[curTurn].emit('yourTurn');
+    }
+  })
+
+  socket.on('fold', () => {
+    console.log("FOLD");
+    socket.data.folded = true;
+    onMove(socket);
+
+  });
+
+  socket.on('call', () => {
+
+    if (socket.data.money < toCall) {
+      console.log("you can't do that.");
+      return;
+    }
+
+    // move their money into the pot.
+    socket.data.money -= toCall;
+    pot += toCall;
+    onMove(socket);
+  })
+
+  socket.on('raise', (amt) => {
+
+    if (socket.data.money < toCall) {
+      console.log("you can't do that.");
+      return;
+    }
+
+    socket.data.money -= amt;
+    pot += amt;
+    toCall += amt;
+    onMove(socket);
+  })
 });
+
 function onMove(socket) {
-    // the client side already checks this, but we shouldn't assume the client is always right.
-    if (socket.id != users[curTurn].id)
-        return;
-    // move around the table until someone isn't folded.
-    do {
-        curTurn++;
-    } while (curTurn < users.length && users[curTurn].data.folded);
-    // update client data.
-    for (var i = 0; i < users.length; i++) {
-        var obj = { money: users[i].data.money, toCall: toCall };
+  // the client side already checks this, but we shouldn't assume the client is always right.
+  if (socket.id != users[curTurn].id) return;
+
+  // move around the table until someone isn't folded.
+  do {
+    curTurn++;
+  } while (curTurn < users.length && users[curTurn].data.folded);
+
+  // update client data.
+  for (let i = 0; i < users.length; i++) {
+    let obj = {money: users[i].data.money, toCall: toCall};
+    users[i].emit('updatePlayer', obj);
+  }
+  
+  // everyone took their turn, advance the game.
+  if (curTurn == users.length) {
+    state++;
+    curTurn = 0;
+
+    // do the flop.
+    if (state == States.flop) {
+      
+      // burn 1.
+      d.drawCard();
+      let flop = [];
+      // deal 3.
+      flop.push(d.drawCard()); flop.push(d.drawCard()); flop.push(d.drawCard());
+
+      console.log("FLOP!!!!");
+      // tell everyone.
+      for (let i = 0; i < users.length; i++) {
+        users[i].emit('flop', flop);
+        let obj = {money: users[i].data.money, toCall: toCall};
+
+        users[i].emit('updateTurn', users[0].data.nickname);
         users[i].emit('updatePlayer', obj);
+      }
+      users[curTurn].emit('yourTurn');
     }
-    // everyone took their turn, advance the game.
-    if (curTurn == users.length) {
-        state++;
-        curTurn = 0;
-        // do the flop.
-        if (state == States.flop) {
-            // burn 1.
-            d.drawCard();
-            var flop = [];
-            // deal 3.
-            flop.push(d.drawCard());
-            flop.push(d.drawCard());
-            flop.push(d.drawCard());
-            console.log("FLOP!!!!");
-            // tell everyone.
-            for (var i = 0; i < users.length; i++) {
-                users[i].emit('flop', flop);
-                var obj = { money: users[i].data.money, toCall: toCall };
-                users[i].emit('updateTurn', users[0].data.nickname);
-                users[i].emit('updatePlayer', obj);
-            }
-            users[curTurn].emit('yourTurn');
-        }
-        // do the turn
-        else if (state == States.turn) {
-            // burn 1.
-            d.drawCard();
-            var turn = d.drawCard();
-            // tell everyone.
-            for (var i = 0; i < users.length; i++) {
-                users[i].emit('turn', turn);
-                var obj = { money: users[i].data.money, toCall: toCall };
-                users[i].emit('updateTurn', users[0].data.nickname);
-                users[i].emit('updatePlayer', obj);
-            }
-            users[curTurn].emit('yourTurn');
-        }
-        else if (state == States.river) {
-            // burn 1.
-            d.drawCard();
-            var river = d.drawCard();
-            // tell everyone.
-            for (var i = 0; i < users.length; i++) {
-                users[i].emit('river', river);
-                var obj = { money: users[i].data.money, toCall: toCall };
-                users[i].emit('updateTurn', users[0].data.nickname);
-                users[i].emit('updatePlayer', obj);
-            }
-            users[curTurn].emit('yourTurn');
-        }
-        console.log("everyone took their turn");
+
+    // do the turn
+    else if (state == States.turn) {
+      // burn 1.
+      d.drawCard();
+      let turn = d.drawCard();
+
+      // tell everyone.
+      for (let i = 0; i < users.length; i++) {
+        users[i].emit('turn', turn);
+        let obj = {money: users[i].data.money, toCall: toCall};
+
+        users[i].emit('updateTurn', users[0].data.nickname);
+        users[i].emit('updatePlayer', obj);
+      }
+      users[curTurn].emit('yourTurn');
     }
-    // if not, tell everyone who's turn it is.
-    else {
-        if (everyoneElseFolded(curTurn)) {
-            console.log(users[curTurn].data.nickname + " wins.");
-            users[curTurn].emit("win");
-            users[curTurn].data.money += pot;
-            pot = 0;
-            var obj = { money: users[curTurn].data.money, toCall: toCall };
-            users[curTurn].emit('updatePlayer', obj);
-            return;
-        }
-        users[curTurn].emit('yourTurn');
-        for (var i = 0; i < users.length; i++) {
-            users[i].emit('updateTurn', users[curTurn].data.nickname);
-        }
+
+    else if (state == States.river) {
+      // burn 1.
+      d.drawCard();
+      let river = d.drawCard();
+
+      // tell everyone.
+      for (let i = 0; i < users.length; i++) {
+        users[i].emit('river', river);
+        let obj = {money: users[i].data.money, toCall: toCall};
+
+        users[i].emit('updateTurn', users[0].data.nickname);
+        users[i].emit('updatePlayer', obj);
+      }
+      users[curTurn].emit('yourTurn');
     }
+    console.log("everyone took their turn");
+  }
+
+  // if not, tell everyone who's turn it is.
+  else {
+
+    if (everyoneElseFolded(curTurn)) {
+      console.log(users[curTurn].data.nickname + " wins.");
+      users[curTurn].emit("win");
+
+      users[curTurn].data.money += pot;
+      pot = 0;
+
+      let obj = {money: users[curTurn].data.money, toCall: toCall};
+      users[curTurn].emit('updatePlayer', obj);
+
+      return;
+    }
+
+    users[curTurn].emit('yourTurn');
+
+    for (let i = 0; i < users.length; i++) {
+      users[i].emit('updateTurn', users[curTurn].data.nickname);
+    }
+  }
 }
+
 function everyoneElseFolded(index) {
-    if (users[index].data.folded)
-        return false;
-    for (var i = 0; i < users.length; i++) {
-        if (i == index)
-            continue;
-        if (!users[i].data.folded)
-            return false;
-    }
-    return true;
+  if (users[index].data.folded) return false;
+
+  for (let i = 0; i < users.length; i++) {
+    if (i == index) continue;
+    if (!users[i].data.folded) return false;
+  }
+
+  return true;
 }
-server.listen(port, function () {
-    console.log('listening on localhost:' + port);
+
+server.listen(port, () => {
+  console.log('listening on localhost:' + port);
 });
